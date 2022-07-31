c599797e69d1a1e81866ce4b55e4c1f93af562b0
Eddie <eulerissmart@gmail.com>
2022-07-31 10:24:07 -0400

    small improvements

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,11 +1,11 @@
-import "../styles/SectionHeader.scss";
-import React from "react";
+import '../styles/SectionHeader.scss';
+import React from 'react';
 
-const SectionHeader = props => {
+const SectionHeader = (props) => {
   return (
     <div className="section__header">
-      <p>{props.title}</p>
-      <h3>{props.subTitle}</h3>
+      <h3>{props.title}</h3>
+      <h4>{props.subTitle}</h4>
     </div>
   );
 };
